3c3ddc7fd4f1428818224b06d0fbe3180906a7ee
alisherKAK <alisher.alish333@gmail.com>
2019-05-09 15:07:46 +0600

    Home Work 16.05.19

---
+++ HomeWork16_05_19.WpfApp/HomeWork16_05_19.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Newtonsoft.Json;
+using HomeWork16_05_19.Models;
+
+namespace HomeWork16_05_19.WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(cityNameTextBox.Text))
+                {
+                    MessageBox.Show("Введите город");
+                    return;
+                }
+
+                string result;
+                using (var client = new WebClient())
+                {
+                    result = client.DownloadString($"http://api.apixu.com/v1/forecast.json?key=59bacc56d0fc4e55bdb144248190605&q={cityNameTextBox.Text}&days=7");
+                }
+
+                var forecast = JsonConvert.DeserializeObject<CityDescription>(result).Forecast;
+                var cards = cardsGrid.Children.OfType<Card>().ToList();
+
+                for (int i = 0; i < cards.Count; i++)
+                {
+                    DateTime forecastDay = Convert.ToDateTime(forecast.Forecastday[i].Date);
+                    var stackPanel = cards[i].Content as StackPanel;
+                    var textBlocks = stackPanel.Children.OfType<TextBlock>().ToList();
+                    var image = stackPanel.Children.OfType<Image>().SingleOrDefault();
+                    image.Source = new ImageSourceConverter().ConvertFromString($"https:{forecast.Forecastday[i].Day.Condition.Icon}") as ImageSource;
+                    textBlocks[0].Text = $"Temperature(C): {forecast.Forecastday[i].Day.AvgtempC}";
+                    textBlocks[1].Text = $"Humidity(%): {forecast.Forecastday[i].Day.Avghumidity}";
+                    textBlocks[2].Text = $"Wind Speed(m/s): {forecast.Forecastday[i].Day.AvgvisKm / 3.6}";
+                    textBlocks[3].Text = $"Date: {forecast.Forecastday[i].Date}";
+                    textBlocks[4].Text = $"Week Day: {forecastDay.DayOfWeek}";
+                    cards[i].Visibility = Visibility.Visible;
+                }
+            }
+            catch(WebException)
+            {
+                MessageBox.Show("Нет такого города");
+            }
+        }
+
+        private void CityNameTextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                try
+                {
+                    if (string.IsNullOrEmpty(cityNameTextBox.Text))
+                    {
+                        MessageBox.Show("Введите город");
+                        return;
+                    }
+
+                    string result;
+                    using (var client = new WebClient())
+                    {
+                        result = client.DownloadString($"http://api.apixu.com/v1/forecast.json?key=59bacc56d0fc4e55bdb144248190605&q={cityNameTextBox.Text}&days=7");
+                    }
+
+                    var forecast = JsonConvert.DeserializeObject<CityDescription>(result).Forecast;
+                    var cards = cardsGrid.Children.OfType<Card>().ToList();
+
+                    for (int i = 0; i < cards.Count; i++)
+                    {
+                        DateTime forecastDay = Convert.ToDateTime(forecast.Forecastday[i].Date);
+                        var stackPanel = cards[i].Content as StackPanel;
+                        var textBlocks = stackPanel.Children.OfType<TextBlock>().ToList();
+                        var image = stackPanel.Children.OfType<Image>().SingleOrDefault();
+                        image.Source = new ImageSourceConverter().ConvertFromString($"https:{forecast.Forecastday[i].Day.Condition.Icon}") as ImageSource;
+                        textBlocks[0].Text = $"Temperature(C): {forecast.Forecastday[i].Day.AvgtempC}";
+                        textBlocks[1].Text = $"Humidity(%): {forecast.Forecastday[i].Day.Avghumidity}";
+                        textBlocks[2].Text = $"Wind Speed(m/s): {forecast.Forecastday[i].Day.AvgvisKm / 3.6}";
+                        textBlocks[3].Text = $"Date: {forecast.Forecastday[i].Date}";
+                        textBlocks[4].Text = $"Week Day: {forecastDay.DayOfWeek}";
+                        cards[i].Visibility = Visibility.Visible;
+                    }
+                }
+                catch (WebException)
+                {
+                    MessageBox.Show("Нет такого города");
+                }
+            }
+        }
+    }
+}
